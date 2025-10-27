e71c6472fa01978cbdad89ad42616a74bad485e2
gstudentmanj <gmanjarrez@student.sierracollege.edu>
2025-10-27 14:22:52 -0700

    Finished main function with all requirements

--- crack.c
+++ crack.c
@@ -24,7 +24,7 @@ char * tryWord(char * plaintext, char * hashFilename)
     if (hash_file == NULL)
     {
 
-        perror("Error opening hash file");
+        perror("Error opening hash file.\n");
         exit(1);   
     }
     // Loop through the hash file, one line at a time.
@@ -59,20 +59,41 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: %s hash_file dict_file\n", argv[0]);
         exit(1);
     }
+
     // Open the dictionary file for reading.
     char *hash_filename = argv[1];
     char *dict_filename = argv[2];
 
+    char line_buffer[PASS_LEN + 2];
+
+    FILE *dict_file = fopen(dict_filename, "r");
+    if (dict_file == NULL)
+    {
+        perror("Error opening dict file.\n");
+        exit(1);
+    }
+    int crackCount = 0;
     // For each dictionary word, pass it to tryWord, which
     // will attempt to match it against the hashes in the hash_file.
-    
+    while (fgets(line_buffer, sizeof(line_buffer), dict_file))
+    {
+        line_buffer[strcspn(line_buffer, "\n")] = '\0';
+
+        char *found = tryWord(line_buffer, hash_filename);
+        if (found)
+        {
+            printf("%s %s\n", found, line_buffer);
+            crackCount++;
+            free(found);
+        }
+    }
     // If we got a match, display the hash and the word. For example:
     //   5d41402abc4b2a76b9719d911017c592 hello
     
     // Close the dictionary file.
-
+    fclose(dict_file);
     // Display the number of hashes that were cracked.
-    
+    printf("Cracked %d Hashes\n", crackCount);
     // Free up any malloc'd memory?
 }
 
